700cd60eeda2a419627b422d5f9897a7e6fdb141
Donald Hutchison <donald.hutchison@toastymofo.net>
2016-06-14 15:19:02 +0200

    Force encoding to be utf 8.

--- lib/wooget/version.rb
+++ lib/wooget/version.rb
@@ -1,5 +1,9 @@
+
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 module Wooget
-  VERSION = "2.0.14".freeze
+  VERSION = "2.0.16".freeze
 
   POST_INSTALL= '
                                 _   _
